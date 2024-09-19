c5495243405b9566fcd9119f64eb1551e81b8d5e
congnc-anz <cong.nguyenchi@anz.com>
2024-09-19 16:26:00 +0700

    merger main

--- fum_flows/models/stage/df_transaction.sql
+++ fum_flows/models/stage/df_transaction.sql
@@ -1,9 +1,5 @@
 WITH
-<<<<<<< HEAD
-DF_Plus AS (
-=======
 df_plus AS (
->>>>>>> main
     SELECT
         transaction_datetime,
         transaction_id,
@@ -21,16 +17,10 @@ df_plus AS (
         COALESCE(transfer_destination_account_bsb, payment_other_entity_account_bsb)        AS dst_bsb_number,
         transaction_amount
     FROM {{ source("dragonfish_transaction_v1", "transaction_anz_plus") }}
-<<<<<<< HEAD
-),
-
-DF_Classic AS (
-=======
     WHERE transaction_datetime > DATE_SUB(DATE_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL 180 DAY)
 ),
 
 df_classic AS (
->>>>>>> main
     SELECT
         transaction_datetime,
         transaction_id,
@@ -48,16 +38,10 @@ df_classic AS (
         COALESCE(transfer_destination_account_bsb, payment_other_entity_account_bsb)        AS dst_bsb_number,
         transaction_amount
     FROM {{ source("dragonfish_transaction_v1", "transaction_anz_classic") }}
-<<<<<<< HEAD
-),
-
-DF_Classic_Mapping AS (
-=======
     WHERE transaction_datetime > DATE_SUB(DATE_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL 180 DAY)
 ),
 
 df_classic_mapping AS (
->>>>>>> main
     SELECT   
         src.transaction_datetime,
         src.transaction_id,
@@ -76,23 +60,6 @@ df_classic_mapping AS (
         src.dst_account_number,
         src.dst_bsb_number,
         src.transaction_amount
-<<<<<<< HEAD
-    FROM DF_Classic src
-    -- Get portfolio+product_group from Reg mapping by cost_centre
-    LEFT JOIN {{ ref("bsb_cc_mapping") }} ccm_cl ON acc_type = 'DDA'
-                                AND RIGHT(src.src_account_number,9) = ccm_cl.account_number 
-                                AND cast(RIGHT(src.src_bsb_number,4) AS NUMERIC) = ccm_cl.bsb 
-                                AND src.src_sub_product_code = ccm_cl.sub_product_code
-                                AND CAST(src.transaction_datetime AS DATETIME) >= ccm_cl.dl__src_eff_from_dttm 
-                                AND CAST(src.transaction_datetime AS DATETIME) <= ccm_cl.dl__src_eff_to_dttm 
-
-    LEFT JOIN {{ ref("reg_data_mapping") }} rm_cl ON src.src_product_code = rm_cl.product_code
-                                AND  src.src_sub_product_code = rm_cl.sub_product_code
-                                AND cast(ccm_cl.cost_centre AS STRING) = rm_cl.cost_centre 
-
-),
-DF_Plus_Mapping AS (
-=======
     FROM df_classic AS src
     -- Get cost_centre 
     LEFT JOIN {{ ref("bsb_cc_mapping") }} AS ccm_cl 
@@ -110,7 +77,6 @@ DF_Plus_Mapping AS (
 ),
 
 df_plus_mapping AS (
->>>>>>> main
     SELECT
         src.transaction_datetime,
         src.transaction_id,
@@ -129,19 +95,6 @@ df_plus_mapping AS (
         src.dst_account_number,
         src.dst_bsb_number,
         src.transaction_amount
-<<<<<<< HEAD
-    FROM DF_Plus src
- -- Get portfolio+product_group from Reg mapping by  mkt_code
-    LEFT JOIN {{ ref("sirius_marketing_code_mapping") }} smc ON smc.account_number = src.src_account_number
-
-    LEFT JOIN {{ ref("reg_data_mapping") }} rm_pl ON concat(src.src_sub_product_code, concat('|',smc.marketing_code)) = rm_pl.sub_product_code
-                                AND cast(RIGHT(src.src_bsb_number,4) AS STRING) = rm_pl.cost_centre
-)
-
-SELECT * FROM DF_Classic_Mapping
-UNION ALL
-SELECT * FROM DF_Plus_Mapping
-=======
     FROM df_plus AS src
     -- Get missing marketing code
     LEFT JOIN {{ ref("sirius_marketing_code_mapping") }} AS smc 
@@ -155,4 +108,3 @@ SELECT * FROM DF_Plus_Mapping
 SELECT * FROM df_classic_mapping
 UNION ALL
 SELECT * FROM df_plus_mapping
->>>>>>> main
