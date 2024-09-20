94f383c8d61b3a9740c5cb47b12b5188444ebf9a
Nemo <anh.nguyenthientuan@anz.com>
2024-09-20 17:15:44 +0700

    relocate-2-legs-processing-logic

--- fum_flows/models/stage/df_transaction.sql
+++ fum_flows/models/stage/df_transaction.sql
@@ -17,7 +17,8 @@ df_plus AS (
         COALESCE(transfer_destination_account_bsb, payment_other_entity_account_bsb)        AS dst_bsb_number,
         transaction_amount
     FROM {{ source("dragonfish_transaction_v1", "transaction_anz_plus") }}
-    WHERE transaction_datetime > DATE_SUB(DATE_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL 180 DAY)
+    WHERE transaction_datetime >= DATE_SUB(DATE_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL 365 DAY)
+    AND transaction_datetime < DATE_TRUNC(CURRENT_TIMESTAMP(), DAY)
 ),
 
 df_classic AS (
@@ -38,7 +39,8 @@ df_classic AS (
         COALESCE(transfer_destination_account_bsb, payment_other_entity_account_bsb)        AS dst_bsb_number,
         transaction_amount
     FROM {{ source("dragonfish_transaction_v1", "transaction_anz_classic") }}
-    WHERE transaction_datetime > DATE_SUB(DATE_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL 180 DAY)
+    WHERE transaction_datetime >= DATE_SUB(DATE_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL 365 DAY)
+    AND transaction_datetime < DATE_TRUNC(CURRENT_TIMESTAMP(), DAY)
 ),
 
 df_classic_mapping AS (
@@ -103,8 +105,84 @@ df_plus_mapping AS (
     LEFT JOIN {{ ref("reg_data_mapping") }} AS rm_pl 
         ON CONCAT(src.src_sub_product_code, CONCAT('|',smc.marketing_code)) = rm_pl.sub_product_code
         AND CAST(RIGHT(src.src_bsb_number,4) AS STRING) = rm_pl.cost_centre
+),
+
+-- Process 2-legs transactions to get the other leg's information
+raw_df_transaction AS (
+    SELECT * FROM df_classic_mapping
+    UNION ALL
+    SELECT * FROM df_plus_mapping
+),
+
+valid_two_legs_puids AS (
+    SELECT DISTINCT transaction_puid, COUNT(transaction_puid) leg_count
+    FROM raw_df_transaction
+    WHERE dst_account_number IS NOT NULL
+    GROUP BY transaction_puid
+    HAVING leg_count = 2
+),
+
+mapping_transaction AS (
+    SELECT
+        org.transaction_datetime
+        ,org.transaction_id
+        ,org.transaction_puid
+        ,org.transaction_src
+        ,org.transaction_type
+        ,org.transaction_status
+        ,org.src_account_number
+        ,org.src_bsb_number
+        ,org.src_portfolio
+        ,org.src_product_group
+        ,org.src_product_code
+        ,org.src_sub_product_code
+        ,org.src_marketing_code
+        ,org.src_term
+        ,map.src_account_number     AS dst_account_number
+        ,map.src_bsb_number         AS dst_bsb_number
+        ,map.src_portfolio          AS dst_portfolio
+        ,map.src_product_group      AS dst_product_group
+        ,map.src_product_code	    AS dst_product_code
+        ,map.src_sub_product_code   AS dst_sub_product_code
+        ,map.src_marketing_code     AS dst_marketing_code
+        ,map.src_term               AS dst_term
+        ,org.transaction_amount
+    FROM raw_df_transaction org
+    INNER JOIN raw_df_transaction map USING (transaction_puid)
+    WHERE org.transaction_puid IN (SELECT transaction_puid FROM valid_two_legs_puids)
+    AND org.src_account_number != map.src_account_number
+),
+
+no_mapping_transaction AS (
+    SELECT
+        transaction_datetime
+        ,transaction_id
+        ,transaction_puid
+        ,transaction_src
+        ,transaction_type
+        ,transaction_status
+        ,src_account_number
+        ,src_bsb_number
+        ,src_portfolio
+        ,src_product_group
+        ,src_product_code
+        ,src_sub_product_code
+        ,src_marketing_code
+        ,src_term
+        ,dst_account_number
+        ,dst_bsb_number
+        ,CAST(NULL AS STRING)	    AS dst_portfolio
+        ,CAST(NULL AS STRING)	    AS dst_product_group
+        ,CAST(NULL AS STRING)	    AS dst_product_code
+        ,CAST(NULL AS STRING)	    AS dst_sub_product_code
+        ,CAST(NULL AS STRING)	    AS dst_marketing_code
+        ,CAST(NULL AS STRING)	    AS dst_term
+        ,transaction_amount
+    FROM raw_df_transaction
+    -- Since transaction_id can be duplicated between classic and plus
+    WHERE CONCAT(transaction_id, transaction_src) NOT IN (SELECT CONCAT(transaction_id, transaction_src) FROM mapping_transaction)
 )
 
-SELECT * FROM df_classic_mapping
+SELECT * FROM mapping_transaction
 UNION ALL
-SELECT * FROM df_plus_mapping
+SELECT * FROM no_mapping_transaction
